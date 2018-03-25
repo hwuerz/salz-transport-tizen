d9849214c12a167c2408fefb8479d1f5f0a610b2
Hendrik Wuerz <h.m.wuerz@web.de>
2018-03-25 12:59:25 +0200

    - added dist folder build

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
@@ -19,7 +23,7 @@ var sassFiles = [
     'sass/*.scss'
 ];
 
-var jsLibs = [ // TODO Exclude config.example
+var jsLibs = [
     'lib/tau/wearable/js/tau.min.js',
     'lib/*.js'
 ];
@@ -38,18 +42,17 @@ var imageFiles = [
     'lib/tau/wearable/theme/blue/images/**/*.png'
 ];
 
+gulp.task('clean', function() {
+    return del([ appFolder, distFolder ]);
+});
+
 // Compile Our Sass
 gulp.task('sass', function() {
     return gulp
         .src(sassFiles)
         .pipe(sass())
         .pipe(concat('all.css'))
-        .pipe(gulp.dest(appFolder))
-
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(gulp.dest(distFolder));
+        .pipe(gulp.dest(appFolder));
 });
 
 // Concatenate & Minify JS Libs
@@ -57,30 +60,25 @@ gulp.task('scripts', function() {
     return gulp
         .src(jsLibs)
         .pipe(concat('lib.js'))
-        .pipe(gulp.dest(appFolder))
-        .pipe(uglify())
-        .pipe(gulp.dest(distFolder));
+        .pipe(gulp.dest(appFolder));
 });
 
 // Copy html files
 gulp.task('html', function() {
     return gulp.src(htmlFiles)
-        .pipe(gulp.dest(appFolder + '/html'))
-        .pipe(gulp.dest(distFolder + '/html'));
+        .pipe(gulp.dest(appFolder + '/html'));
 });
 
 // Copy config files
 gulp.task('tizen', function() {
     return gulp.src(tizenFiles)
-        .pipe(gulp.dest(appFolder))
-        .pipe(gulp.dest(distFolder));
+        .pipe(gulp.dest(appFolder));
 });
 
 // Copy images files
 gulp.task('images', function() {
     return gulp.src(imageFiles)
-        .pipe(gulp.dest(appFolder + '/images'))
-        .pipe(gulp.dest(distFolder + '/images'));
+        .pipe(gulp.dest(appFolder + '/images'));
 });
 
 
@@ -92,7 +90,22 @@ gulp.task("typescript", function () {
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest(appFolder));
-    // TODO Add dist folder
+});
+
+// Minify and uglify files in appFolder to distFolder
+gulp.task('dist', [], function() {
+    var jsFilter = filter('**/*.js', {restore: true});
+    var cssFilter = filter('**/*.css', {restore: true});
+    return gulp.src(appFolder + '/**/*')
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+
+        .pipe(cssFilter)
+        .pipe(cleanCSS())
+        .pipe(cssFilter.restore)
+
+        .pipe(gulp.dest(distFolder));
 });
 
 // Watch Files For Changes
@@ -106,6 +119,13 @@ gulp.task('watch', function() {
 
 // Build the full app
 gulp.task('build', ['sass', 'scripts', 'html', 'tizen', 'images', 'typescript']);
+
+// Build the full app for deployment
+gulp.task('build-dist', function(done) {
+    runSequence('clean', 'build', 'dist', function() {
+        done();
+    });
+});
 
 // Default Task
 gulp.task('default', ['build', 'watch']);
